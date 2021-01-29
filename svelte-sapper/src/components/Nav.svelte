3417ac6082a030981826a1751f242c0bb298420b
Dakota Savage <djsavage2@bsu.edu>
2021-01-28 23:27:35 -0500

    alight right navbar

--- svelte-sapper/src/components/Nav.svelte
+++ svelte-sapper/src/components/Nav.svelte
@@ -7,6 +7,9 @@
 		border-bottom: 1px solid rgba(255,62,0,0.1);
 		font-weight: 300;
 		padding: 0 1em;
+		margin-left: auto;
+		display: block;
+		width: fit-content;
 	}
 
 	ul {
@@ -23,6 +26,7 @@
 
 	li {
 		display: block;
+		position: relative;
 		float: left;
 	}
 
@@ -46,15 +50,21 @@
 		padding: 1em 0.5em;
 		display: block;
 	}
+
+	div {
+		float: right;
+	}
 </style>
 
 <nav>
 	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
+		<div>
+			<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
+			<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
 
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
+			<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+				the blog data when we hover over the link or tap it on a touchscreen -->
+			<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
+		</div>
 	</ul>
 </nav>
